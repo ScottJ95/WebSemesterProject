5ae3a4ccd9ca302b48a8aa3b8c332d14528a0134
Kershaw <kersha02@elvix2.rowan.edu>
2017-04-11 14:39:22 -0400

    Added login.js

--- Public/login.js
+++ Public/login.js
@@ -1,3 +1,17 @@
 function login() {
-	document.getElementById("demo").innerHTML = "Hello World!";
+	var email = document.getElementById("emailBox").value;
+	var password = document.getElementById("passwordBox").value;
+
+	if(email=="")
+	{
+		document.getElementById("popup").innerHTML = "Please enter a valid email";
+	}
+	if(password=="")
+	{
+		document.getElementById("popup").innerHTML = "Please enter your password";
+	}
+
+
+
+	
 }
