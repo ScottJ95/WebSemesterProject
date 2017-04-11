6217779a9df40f9cafde7025421cd588f65e7d91
Kershaw <kersha02@elvix2.rowan.edu>
2017-04-11 14:45:00 -0400

    Added login.php

---
+++ Public/login.php
@@ -0,0 +1,11 @@
+<?php
+require_once('DBFuncs.php');
+require_once('/home/jefferys0/source_html/web/WebSemesterProject/Connect.php');
+
+$dbh = ConnectDB();
+
+?>
+
+<script type="text/javascript">
+
+
