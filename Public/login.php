176a1d6ae4c34e77ec26d033426b2a7149b0eeeb
Kershaw <kersha02@elvix2.rowan.edu>
2017-04-11 14:52:21 -0400

    Worked on login.php

--- Public/login.php
+++ Public/login.php
@@ -8,4 +8,47 @@ $dbh = ConnectDB();
 
 <script type="text/javascript">
 
+function checkName(){
 
+        var userName = $("#userName").val();
+
+        console.log(groupName); //Debugging. Comment out.
+
+        if(userName) { //If it's not null, let's check it.
+                //Jquery to setup AJAX we can give it a bunch of stuff
+                //type: post or get?
+                //url: What script do we run?
+                //data: What data are we sending?
+                //success or failure callbacks
+                //This is equivalent to jquery.post(), but this made more sense to me.
+                //https://api.jquery.com/jquery.post/
+                $.ajax({ 
+                   type: 'post',
+                   url:  'checkGroup.php',
+                   data: {
+                        group_name:groupName,
+                   },
+                
+                   success: function (response) {
+                           //Call was successful, so do this function
+                           //First, set the name_status html to the response.
+                        $( '#name_status').html(response);
+                        //Check the response so we can return the check
+                        if(response == "OK") {
+                                return true;
+                        }
+
+                        else {
+                                return false;
+                        }
+                }
+                });//End Ajax
+        } //End If
+
+        else //Nothing typed into the group name
+        {
+                $( '#name_stats').html("");
+                return false;
+        }
+}
+</script>
