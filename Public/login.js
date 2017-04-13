90c434d968fcffbdf39c68096df4a388c5afd6e2
Jacob Kershaw <kersha02@elvix2.rowan.edu>
2017-04-13 15:14:06 -0400

    Started working on password Changes

--- Public/login.js
+++ Public/login.js
@@ -40,5 +40,47 @@ function newUser(){
 }
 
 function forgotPassword(){
-	
+	window.location.href = "comfirmEmail.html";	
 }
+
+function comfirmCancel(){
+	window.location.href = "login.html";
+}
+
+function comfirmSend(){
+
+	var email = document.getElementById("emailBox").value;
+
+        if(email=="")
+        {
+                document.getElementById("popup").innerHTML = "Please enter your email";
+        }
+	else
+	{
+	
+	$.ajax({
+                   type: 'POST',
+                   url:  'DBFuncs.php',
+                   data: { functionName:'checkEmail',argument:email },
+
+                   success: function (response) {
+                           if(response == 0){
+                                document.getElementById("popup").innerHTML="Email address not found in system. Please enter a valid email.";
+                                }
+                           else
+                           {
+                                  //Send Email
+                           }
+                   }
+
+                });	
+	}
+}
+function passwordChange(){
+
+}
+
+function passwordCancel(){
+	window.location.href = "login.html";
+}
+
