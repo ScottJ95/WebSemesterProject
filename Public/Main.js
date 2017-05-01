8d66b8429a672f58e42f5ade9c3e8840b17b2ed6
Scott A. Jeffery <jefferys0@elvix2.rowan.edu>
2017-04-30 21:08:51 -0400

    Removed group images.

--- Public/Main.js
+++ Public/Main.js
@@ -38,8 +38,8 @@ function Groups(evt,x){
                 var groupData = JSON.parse(response);
                 for(i = 0;i<groupData.length;i++){
                     document.getElementById("groupList").innerHTML+="<div class=\"group\"><a onclick=\"moveToChat("
-                    +groupData[i].group_ID+");\"><div class = \"groupButton\"><img class = \"groupImage\" alt=\"groupIcon\" src=\""
-                    +"\"><div class = \"chatName\">"+groupData[i].group_name+"</div><div class=\"chatSize\">"+groupData[i].group_numUsers+"</div><div class = \"chatDesc\">"
+                    +groupData[i].group_ID+");\"><div class = \"groupButton\">"
+                    +"<div class = \"chatName\">"+groupData[i].group_name+"</div><div class=\"chatSize\">"+groupData[i].group_numUsers+"</div><div class = \"chatDesc\">"
                     + groupData[i].group_description+"</div></div>"
                     +"</a><a onclick=\"\" <div class=\"leaveButton\">Leave</div></a></div>";
                 }
